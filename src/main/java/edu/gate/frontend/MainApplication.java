2f9098733934152df543ef1b867c0572b644dc1c
ge38wiv <ge38wiv@mytum.de>
2023-06-27 16:00:37 +0200

    Clean-up in the program and ready to merge to the main branch.

--- src/main/java/edu/gate/frontend/MainApplication.java
+++ src/main/java/edu/gate/frontend/MainApplication.java
@@ -1,33 +1,33 @@
 package edu.gate.frontend;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import java.io.File;
-import java.io.IOException;
 
+/**
+ * MainApplication extended with the Application class for JavaFX. It is the entrance class for the frontend/user to
+ * the system and the start of the whole program. It is important to start via Maven run and not Main run, as described
+ * in the README.txt.
+ */
 public class MainApplication extends Application {
-
+    /**
+     * The main method is for JavaFX purposes initialized with a command.
+     * @param args Java execution arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Comments after completion!
+     * @param primaryStage is the basic and first stage object for the JavaFX structure
+     */
     @Override
-    public void start(Stage stage) {
-        stage.setTitle("Configuration Interface for Motor Testing");
-        stage.setHeight(400);
-        stage.setWidth(600);
-        stage.setX(450);
-        stage.setY(200);
-
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root);
-        scene.setFill(Color.DARKGREY);
-
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        Group root1 = new Group();
+        Scene scene = new Scene(root1);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
