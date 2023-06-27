e868371965d15ab104bb9113faa5ee3c9ad888b0
ge38wiv <ge38wiv@mytum.de>
2023-06-27 20:25:59 +0200

    JavaFX experimentations and first integrations. It was also changed to use SceneBuilder for operating designs.

--- src/main/java/edu/gate/frontend/MainApplication.java
+++ src/main/java/edu/gate/frontend/MainApplication.java
@@ -1,9 +1,13 @@
 package edu.gate.frontend;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 /**
  * MainApplication extended with the Application class for JavaFX. It is the entrance class for the frontend/user to
@@ -25,9 +29,28 @@ public class MainApplication extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
-        Group root1 = new Group();
-        Scene scene = new Scene(root1);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        /* For error handling, the implementation of a try-catch structure is helpful */
+        try {
+
+            /* JavaFX root1 element for the first group of Scene-Graph elements (element grouping - low) */
+            //Group root1 = new Group(); /* We are using SceneBuilder */
+            Parent root1 = FXMLLoader.load(new File(
+                        "src/main/java/edu/gate/frontend/SceneCreator.fxml")
+                    .toURI().toURL()); /* load fxml file (the URI step is conventional necessary)*/
+
+            /* JavaFX scene including the first group for the scene creation (visualisation content - middle) */
+            Scene scene = new Scene(root1);
+            scene.setFill(Color.web("#121212")); /* set background color */
+
+            /* JavaFX stage as the main window for the whole application (application window - high) */
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Motor Quality Control Application"); /* set title */
+            primaryStage.setFullScreen(true); /* set window to full screen */
+            primaryStage.show();
+
+        } catch (Exception fx) { /* Throw a general text and additional information */
+            System.out.println("There is an error in the JavaFX application or in one of the related applications!");
+            fx.printStackTrace();
+        }
     }
 }
