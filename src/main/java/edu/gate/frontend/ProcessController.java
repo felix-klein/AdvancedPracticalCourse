9bd11b22e64774a8529218e14be86d2cfc069b66
ge38wiv <ge38wiv@mytum.de>
2023-09-04 17:07:51 +0200

    Partly working web view with the wrong browser. The Overall view is correct.

--- src/main/java/edu/gate/frontend/ProcessController.java
+++ src/main/java/edu/gate/frontend/ProcessController.java
@@ -1,4 +1,18 @@
 package edu.gate.frontend;
 
-public class ProcessController extends ComboController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProcessController extends ComboController implements Initializable {
+    @FXML
+    WebView webView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        webView.getEngine().load("https://cpee.org/flow/?monitor=https://cpee.org/flow/engine/21665/");
+    }
 }
